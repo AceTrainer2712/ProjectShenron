e1e5e7bad67c6c80da9c9e8b976e4883c9ddb7a6
AceTrainer2712 <acetrainer2712@gmail.com>
2017-05-29 11:05:27 +0530

    attempt to add init message in discord channel

--- shenron.js
+++ shenron.js
@@ -4,11 +4,26 @@ const config = require('./config.json');
 
 client.login(config.token);
 
+
 /* #general CID in ACEBOTTEST: '303554856948465664' */
 
 client.on('ready', () => {
-	console.log('I have been summoned.');
+	var y = 'I have been summoned.';
+	console.log(y);
+	
+	const channel = client.channels.get("name","general").id;
+
+	client.channels.get("id",channel).send('I am Shenron. Speak your wishes.');
+	
 });
+
+function output(error) {
+	if (error) {
+		console.log('There was an error in initialising');
+	} else{
+		console.log('Initialising complete.')
+	}
+}
 
 client.on('message', (message) => {
   if (message.content.startsWith('ping')) {
