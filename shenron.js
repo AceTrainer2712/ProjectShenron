4b6b881889117a9906db33d1a265000f6a960f45
Ace <acetrainer2712@gmail.com>
2017-09-29 21:47:01 -0400

    Added colour change feature

--- shenron.js
+++ shenron.js
@@ -23,15 +23,30 @@ function output(error) {
 
 client.on('message', (message) => {
   if(!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  if(message.content.startsWith(config.prefix + 'ping')) {
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(command === 'ping') {
     message.channel.send('pong!');
   }
 
-  if(message.content.startsWith(config.prefix + 'cake')) {
+  if(command === 'cake') {
+
   	let cakes = message.content.split(' ').slice(1);
   	let target = cakes[0];
   	//giveCake.func(target);
   	message.channel.send(':cake:  |  ** ' + message.author + ' has given ' + target + ' a slice of cake! **');
   }
-});
+
+    if(command === 'setcolor') {
+    //select hex code if command is s!setcolor FF00000 RoleName
+    let hex = args[0];
+    let author_role = args.slice(1).join(" ");
+    let roleid = message.guild.roles.find("name", author_role);
+    
+    message.guild.roles.find("name", author_role).setColor(hex)
+      .then(r => console.log(`Set color of role ${r}`))
+      .catch(console.error);
+
+    message.channel.send('Your color has been changed, ' + message.author + '! Enjoy your new colour :)');
+  }
